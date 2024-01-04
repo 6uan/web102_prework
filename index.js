50847dd013cdd04f6ff20df661cdc70002267efa
6uan <6uannn@gmail.com>
2024-01-03 22:26:34 -0500

    Challenge 4 / 7

--- index.js
+++ index.js
@@ -75,19 +75,37 @@ console.log(addGamesToPage(GAMES_JSON));
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContribution = GAMES_JSON.reduce ( (acc, game) => {
+    return acc + game.backers;
+}, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
-
+contributionsCard.innerHTML = `
+    <div>${totalContribution.toLocaleString('en-US')}</div>
+`
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
-// set inner HTML using template literal
+const totalRaised = GAMES_JSON.reduce ( (acc, game) => {
+    return acc + game.pledged;
+}, 0)
 
+// set inner HTML using template literal
+raisedCard.innerHTML = `
+    <div>$${totalRaised.toLocaleString('en-US')}</div>
+`
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+
+const totalGames = GAMES_JSON.reduce( (acc, game) => {
+        return acc + 1;
+}, 0)
+
+gamesCard.innerHTML = `
+    <div>${totalGames.toLocaleString('en-US')}</div>
+`
 
 
 /*************************************************************************************
