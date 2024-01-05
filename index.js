ac67b93b61d0ea810609f47b19e2f91a6480f0e2
6uan <6uannn@gmail.com>
2024-01-04 21:29:36 -0500

    New Website Style

--- index.js
+++ index.js
@@ -237,7 +237,12 @@ const [firstGame, secondGame, ...remainingGames] = sortedGames;
 let topGameDiv = document.createElement("div");
 
 topGameDiv.innerHTML = `
+<img class = "game-img-2" src = "${firstGame.img}">
 <p>${firstGame.name}</p>
+<p>${firstGame.pledged.toLocaleString("en-US")} / ${firstGame.goal.toLocaleString("en-US")}</p>
+<p>Over by ${(firstGame.pledged - firstGame.goal).toLocaleString("en-US")}
+<svg xmlns="http://www.w3.org/2000/svg" height="17" width="15" viewBox="0 0 384 512"><path fill="#53ea55" d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>
+</p>
 `
 firstGameContainer.append(topGameDiv);
 
@@ -246,6 +251,11 @@ firstGameContainer.append(topGameDiv);
 let runnerUpGameDiv = document.createElement("div");
 
 runnerUpGameDiv.innerHTML = `
+<img class = "game-img-2" src = "${secondGame.img}">
 <p>${secondGame.name}</p>
+<p>${secondGame.pledged.toLocaleString("en-US")} / ${secondGame.goal.toLocaleString("en-US")}</p>
+<p>Over by ${(secondGame.pledged - secondGame.goal).toLocaleString("en-US")}
+<svg xmlns="http://www.w3.org/2000/svg" height="17" width="15" viewBox="0 0 384 512"><path fill="#53ea55" d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>
+</p>
 `
 secondGameContainer.append(runnerUpGameDiv);
